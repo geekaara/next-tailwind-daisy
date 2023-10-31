4eb223822c53752b2daf02b2f4f4d6536f3fe3aa
Aaradhanah Appalo Eleven <43980950+geekaara@users.noreply.github.com>
2023-11-01 00:14:45 +1100

    users app

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
+import Link from 'next/link'
+import ProductCart from './components/ProductCart'
 
 export default function Home() {
   return (
     <main>
       <h1>Issue Tracker</h1>
+      <Link href="/users">Users</Link>
+      <ProductCart />
     </main>
   )
 }
---
+++ app/users/page.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+interface User {
+    id: number;
+    name: string;
+    email: string;
+}
+
+const UsersPage = async() => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users',
+    {cache: 'no-store'}
+    )
+    const users: User[] = await response.json();
+  return (
+    <>
+    <h1>Users</h1>
+    <p>{new Date().toLocaleTimeString()}</p>
+    <table className='table table-zebra'>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            {users.map(user => <tr key={user.id}><td>{user.name}</td>
+            <td>{user.email}</td></tr>)}
+        </tbody>
+    </table>
+    </>
+  )
+}
+
+export default UsersPage
